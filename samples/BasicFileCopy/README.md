b9382b22a099e853d70ab6eb24cdb18261e2c71d
jixer <me@bloggedbychris.com>
2014-04-08 00:00:40 -0700

    Added Basic File Copy example

---
+++ samples/BasicFileCopy/README.md
@@ -0,0 +1,3 @@
+Basic File Copy Example
+=======================
+
---
+++ samples/BasicFileCopy/heatScript.ps1
@@ -0,0 +1,8 @@
+﻿#Example of heat usage for directory:
+#>>heat dir ".\My Files" -gg -sfrag -template:fragment -out directory.wxs
+
+$heat = "C:\Program Files (x86)\WiX Toolset v3.8\bin\heat.exe";
+$publishDir = "C:\drop"; 
+$outputWxs = "C:\Users\myers_000\Desktop\wix-examples\samples\BasicFileCopy\Files.wxs";
+
+&$heat dir $publishDir -cg "CMP_InstallFiles" -gg -g1 -sfrag -template fragment -var var.DropDir -out $outputWxs
